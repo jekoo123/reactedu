f8ddb7f0b4e6bcb7bcd6c90984d9245dde8b9c1f
jekoo123 <peter6998@naver.com>
2024-05-08 15:23:38 +0900

    feat

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">asdfasdfsadf</div>;
+  return (
+    <div className="App">
+      asdfasdfsadf
+      <div>hi</div>
+    </div>
+  );
 }
 
 export default App;
